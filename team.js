21c6223bf1d6b8287b2be95a325a96562aaf0929
Marcello Sciulara <marcello.sciulara2014@gmail.com>
2024-01-04 13:52:05 +0100

    correto e ordinato in ordine alfabetico

--- team.js
+++ team.js
@@ -1,1 +1,17 @@
-const persons = [ name = 'Marcello', surname = 'Sciulara', age = 25, city = 'Mezzojuso', hobby = 'Leggere', favoriteFood = 'Pizza', favoriteVideoGame = 'DS3', favoriteFilm = 'V per vendetta', favoriteBook = 'Nella quarta dimensione', petName = '' ];
+const persons = [ { name : 'Marcello', surname : 'Sciulara', age : 25, city : 'Mezzojuso', hobby : 'Leggere', favoriteFood : 'Pizza', favoriteVideoGame : 'DS3', favoriteFilm : 'V per vendetta', favoriteBook : 'Nella quarta dimensione', petName : '' }, ];
+
+function alphabeticalOrder(a, b){
+    if(a.surname < b.surname) {
+        return -1;
+    } else if (a.surname > b.surname) {
+        return 1;
+    } else {
+        return a.name.localeCompare(b.name)
+    }
+}
+
+persons.sort(alphabeticalOrder);
+
+persons.forEach(function(person) {
+    console.log(`${person.surname} ${person.name}`);
+  });
