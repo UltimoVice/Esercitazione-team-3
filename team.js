542fc2df162f9a65b24184ac8f147ade9e065e92
Marcello Sciulara <152990614+UltimoVice@users.noreply.github.com>
2024-01-08 12:42:34 +0100

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,5 @@
-const persons = [];
+const persons = [{name : "Riccardo", surname : "Grassi", age : 27, city : "Livorno", hobby : "Gym", favoriteFood : "Cacciucco", favoriteVideoGame : "Skyrim", favoriteFilm : "Old boy",
+favoriteBook : "Neuromante", petName : "Ecate"}]
+
+const totalAge = persons.reduce((acc, person) => acc + person.age, 0);
+const middleAge = totalAge / persons.length;
