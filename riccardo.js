36dd2c0165fe54b6dab28ad8eddd1657918d58be
Marcello Sciulara <152990614+UltimoVice@users.noreply.github.com>
2024-01-08 12:44:05 +0100

    Delete riccardo.js

--- riccardo.js
+++
@@ -1,5 +0,0 @@
-const persons = [{name : "Riccardo", surname : "Grassi", age : 27, city : "Livorno", hobby : "Gym", favoriteFood : "Cacciucco", favoriteVideoGame : "Skyrim", favoriteFilm : "Old boy",
-favoriteBook : "Neuromante", petName : "Ecate"}]
-
- const totalAge = persons.reduce((acc, person) => acc + person.age, 0);
- const middleAge = totalAge / persons.length;
