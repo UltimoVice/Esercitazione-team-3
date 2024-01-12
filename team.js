734cf365b5444264261f1d82d54435fc66fb3b61
mikedarmand <mikedarmand@users.noreply.github.com>
2024-01-12 16:17:53 +0100

    aggiunto esercizio mike

--- team.js
+++ team.js
@@ -1,23 +1,38 @@
 const persons = [
                  {name : "Riccardo", surname : "Grassi", age : 27, city : "Livorno", hobby : "Gym", favoriteFood : "Cacciucco", favoriteVideoGame : "Skyrim", favoriteFilm : "Old boy", favoriteBook : "Neuromante", petName : "Ecate"},
-                 {name : 'Marcello', surname : 'Sciulara', age : 25, city : 'Mezzojuso', hobby : 'Leggere', favoriteFood : 'Pizza', favoriteVideoGame : 'DS3', favoriteFilm : 'V per vendetta', favoriteBook : 'Nella quarta dimensione', petName : '' }
+                 {name : 'Marcello', surname : 'Sciulara', age : 25, city : 'Mezzojuso', hobby : 'Leggere', favoriteFood : 'Pizza', favoriteVideoGame : 'DS3', favoriteFilm : 'V per vendetta', favoriteBook : 'Nella quarta dimensione', petName : '' },
+                {name :"mike", surname : "darmand", age : 27, city : "reggio calabria", hobby : "nothing", favoriteFood : "frutta", favoriteVideoGame : "street fighter", favoriteFilm : " vikings", favoriteBook : " ", petName : "gazelle"},
+                 
+                
+
+                
+                
+                
                 ]
 
-const totalAge = persons.reduce((acc, person) => acc + person.age, 0);
-const middleAge = totalAge / persons.length;
+                function proprietarioAnim(){
+                 return persons.filter(person=>(person.petName)).map(person=>`${person.name} ${person.surname} ha un animale che si chiama ${person.petName}  `)
 
-function alphabeticalOrder(a, b){
-    if(a.surname < b.surname) {
-        return -1;
-    } else if (a.surname > b.surname) {
-        return 1;
-    } else {
-        return a.name.localeCompare(b.name)
-    }
-}
+                }
+                console.log(proprietarioAnim())
 
-persons.sort(alphabeticalOrder);
+                
 
-persons.forEach(function(person) {
-    console.log(`${person.surname} ${person.name}`);
-  });
+// const totalAge = persons.reduce((acc, person) => acc + person.age, 0);
+// const middleAge = totalAge / persons.length;
+
+// function alphabeticalOrder(a, b){
+//     if(a.surname < b.surname) {
+//         return -1;
+//     } else if (a.surname > b.surname) {
+//         return 1;
+//     } else {
+//         return a.name.localeCompare(b.name)
+//     }
+// }
+
+// persons.sort(alphabeticalOrder);
+
+// persons.forEach(function(person) {
+//     console.log(`${person.surname} ${person.name}`);
+//   });
