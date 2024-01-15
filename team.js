b99193b7b51ea7411e7adb06b176ec6fa06dac32
Kathreence <espiritu.kathreen@gmail.com>
2024-01-15 14:19:39 +0100

    modified Kathreen part

--- team.js
+++ team.js
@@ -2,7 +2,7 @@ const persons = [
   { name : 'Marcello', surname : 'Sciulara', age : 25, city : 'Mezzojuso', hobby : 'Leggere', favoriteFood : 'Pizza', favoriteVideoGame : 'Ds3', favoriteFilm : 'V per vendetta', favoriteBook : 'Nella quarta dimensione', petName : '' },
   { name : "Riccardo", surname : "Grassi", age : 27, city : "Livorno", hobby : "Gym", favoriteFood : "Cacciucco", favoriteVideoGame : "Skyrim", favoriteFilm : "Old boy", favoriteBook : "Neuromante", petName : "Ecate"},
   { name :"mike", surname : "darmand", age : 27, city : "reggio calabria", hobby : "nothing", favoriteFood : "frutta", favoriteVideoGame : "street fighter", favoriteFilm : " vikings", favoriteBook : " ", petName : "gazelle"},
-  {name : 'Kathreen', surname : 'Espiritu', age : 27, city : 'Milano', hobby : 'Giadinaggio', favoriteFood : 'Lasagna', favoriteVideoGame : '', favoriteFilm : 'Titanic', favoriteBook : '', petName : '' },
+  { name : 'Kathreen', surname : 'Espiritu', age : 27, city : 'Milano', hobby : 'Giadinaggio', favoriteFood : 'Lasagna', favoriteVideoGame : '', favoriteFilm : 'Titanic', favoriteBook : '', petName : '' },
 ];
 
 
@@ -21,7 +21,7 @@ function middleAge(persons) {
     return console.log(totalAge / persons.length)
  }
 
- middleAge(persons)
+middleAge(persons)
   
 // Mike
 function proprietarioAnim(){
@@ -31,5 +31,5 @@ function proprietarioAnim(){
 console.log(proprietarioAnim())
 
 // Kathreen
-const ageSorted = (array = []) => array.sort((a, b) => {return a.age - b.age})
-console.log(ageSorted)
+const ageSorted = (array = []) => array.sort((a, b) => {return a.age - b.age}).map(person =>`${person.name}, ${person.age}`)
+console.log(ageSorted(persons))
